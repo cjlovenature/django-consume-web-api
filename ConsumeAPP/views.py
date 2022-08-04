c3e4675c0d3a875e030850741b0da766036a84a2
Chandan K Jha <94809148+cjlovenature@users.noreply.github.com>
2022-08-04 14:23:01 +0530

    Update views.py

--- ConsumeAPP/views.py
+++ ConsumeAPP/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 '''def home(request):
-    url= 'https://api.openweathermap.org/data/2.5/weather?q=London&APPID=59d368eb73118e986bacb845e45dbec5'
+    url= 'https://api.openweathermap.org/data/2.5/weather?q=London&APPID=XXXXXXXXXXXXXXXXXXX'
     response = requests.get(url)
     data = response.json()
     return JsonResponse(data,safe=False)'''
